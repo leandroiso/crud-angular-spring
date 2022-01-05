4b57d61d9c2ca07fc8c222933a9f262dfdb46701
Leandroiso <leandroinacio@gmail.com>
2022-01-04 21:09:41 -0300

    primeiro commit

---
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+
+import { Course } from '../model/course';
+import { CourseService } from '../services/course.service';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses$: Observable<Course[]>;
+  //courses: Course[] = [];
+  displayedColumns = ['name','category'];
+
+  //coursesService: CourseService;
+
+  constructor(
+    private coursesService: CourseService,
+    public dialog: MatDialog
+    ) {
+    //this.courses = [];
+    //this.coursesService = new CourseService();
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar curso. ');
+        return of ([])
+      })
+    );
+
+    //this.coursesService.list().subscribe(courses => this.courses = courses);
+
+   }
+
+   onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ crud-spring/src/main/java/com/leandro/controller/CourseController.java
@@ -0,0 +1,25 @@
+package com.leandro.controller;
+
+import java.util.List;
+
+import com.leandro.model.Course;
+import com.leandro.repository.CourseRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+}
